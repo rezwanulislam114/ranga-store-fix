a1cb8cae9022060b84beef84137f9594e748dfa4
Rezwanul Islam Rifat <rezwanulislam114>
2021-09-13 01:45:54 +0600

    outlook changed

--- js/app.js
+++ js/app.js
@@ -12,16 +12,16 @@ const showProducts = (products) => {
     console.log(product);
     const image = product.image;
     const div = document.createElement("div");
-    div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
+    div.classList.add("single-product");
+    div.innerHTML = `<div">
       <div>
     <img class="product-image" src=${image}></img>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="cart-button"><i class="fas fa-shopping-cart"></i>Add to cart</button>
+      <button id="details-btn" class="detail-button"><i class="fas fa-info-circle"></i>Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
@@ -30,7 +30,7 @@ let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
-
+  updateTotal();
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
 };
@@ -43,15 +43,16 @@ const getInputValue = (id) => {
 
 // main price update function
 const updatePrice = (id, value) => {
+  console.log(id, value);
   const convertedOldPrice = getInputValue(id);
-  const convertPrice = parseFloat(value);
+  const convertPrice = value;
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total;
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = value;
 };
 
 // update delivery charge and total Tax
